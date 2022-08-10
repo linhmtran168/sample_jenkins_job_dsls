19516579b938cd26ebc63b07080911241805d1d6
Linh Tran <linh.mtran168@live.com>
2022-08-10 15:55:16 +0700

    fix views

--- jobs/sample_views.groovy
+++ jobs/sample_views.groovy
@@ -1,16 +1,14 @@
-nestedView('Generated Jobs') {
-    views {
-        listView('overview') {
-            jobs {
-                regex(/test-job.*/)
-            }
-            columns {
-                status()
-                weather()
-                name()
-                lastSuccess()
-                lastFailure()
-            }
+views {
+    listView('Generated Jobs') {
+        jobs {
+            regex(/test-job.*/)
+        }
+        columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
         }
     }
 }
